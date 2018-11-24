3c8cfed5c9db49ec8ef166a21c62b0aa4e8780d7
Daniel Cuesta <danicuesta00@gmail.com>
2018-11-24 11:27:10 +0100

    first commit

---
+++ 1-temperature-sensor/modules/temperature-sensor/hubmanager.py
@@ -0,0 +1,23 @@
+import iothub_client
+# pylint: disable=E0611
+from iothub_client import IoTHubModuleClient, IoTHubTransportProvider
+
+# messageTimeout - the maximum time in milliseconds until a message times out.
+# The timeout period starts at IoTHubClient.send_event_async.
+# By default, messages do not expire.
+MESSAGE_TIMEOUT = 10000
+
+# Choose HTTP, AMQP or MQTT as transport protocol.  Currently only MQTT is supported.
+PROTOCOL = IoTHubTransportProvider.MQTT
+
+class HubManager(object):
+
+    def __init__(
+            self,
+            protocol=IoTHubTransportProvider.MQTT):
+        self.client_protocol = protocol
+        self.client = IoTHubModuleClient()
+        self.client.create_from_environment(protocol)
+
+        # set the time until a message times out
+        self.client.set_option("messageTimeout", MESSAGE_TIMEOUT)
